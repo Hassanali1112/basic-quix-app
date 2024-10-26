e01ed5c076a0b166da91b95cec09ae9fbf716e4a
hassan ali <hassanali0111272@gmail.com>
2024-10-26 09:11:02 -0700

    bug fixed

--- assignment.js
+++ assignment.js
@@ -2,33 +2,54 @@ const parentElem = document.querySelector(".bottom-section");
 const correctAns = document.querySelector(".correct-ans");
 const wrongAns = document.querySelector(".wrong-ans");
 const finalScore = document.querySelector(".score");
-const quizSection = document.querySelector(".bottom-section");
+
 let score = 0;
 let wrong = 0;
 let NumberOfClicks = 0;
-parentElem.addEventListener("click", function (e){
-  e.preventDefault();
-  NumberOfClicks++;
-  if(e.target.classList.contains("correct")){
-     e.target.parentElement.parentElement.style.display = "none";
-    console.log(e.target.parentElement.parentElement)
+// parentElem.addEventListener("click", function (e){
+//   e.preventDefault();
+//   NumberOfClicks++;
+//   if(e.target.classList.contains("correct")){
+//      e.target.parentElement.parentElement.style.display = "none";
+//     score++;
+//     correctAns.textContent = score;
+
+//   } else {
+//     wrong++;
+//      e.target.parentElement.parentElement.style.display = "none";
+//        console.log(e.target.parentElement.parentElement);
+//     wrongAns.textContent = wrong;
+//   }
+//   if (NumberOfClicks === parentElem.children.length) {
+
+//     endQuiz();
+//   }
+// })
+// function endQuiz (){
+//   let scoreInPercentage = (score / parentElem.children.length)* 100;
+//   finalScore.classList.add("customClass");
+//   finalScore.textContent = `${scoreInPercentage}%`
+//   quizSection.style.display = "none";
+// }
+parentElem.addEventListener("click", function (e) {
+  if (e.target.classList.contains("correct")) {
+    e.target.parentElement.parentElement.classList.add("filter");
     score++;
     correctAns.textContent = score;
+    NumberOfClicks++;
+  } else{
+    if(e.target.classList.contains("wrong")){
+      e.target.parentElement.parentElement.classList.add("filter")
+      wrong++;
+      wrongAns.textContent = wrong;
+      NumberOfClicks++;
+    }
+  }
+  if(NumberOfClicks === parentElem.children.length){
+    let scoreInPercentage = (score / parentElem.children.length)* 100;
+      finalScore.classList.add("customClass");
+      finalScore.textContent = `${scoreInPercentage}%`
+      parentElem.style.display = "none";
+  }
 
-  } else {
-    wrong++;
-     e.target.parentElement.parentElement.style.display = "none";
-       console.log(e.target.parentElement.parentElement);
-    wrongAns.textContent = wrong;
-  }
-  if (NumberOfClicks === parentElem.children.length) {
-   
-    endQuiz();
-  }
 })
-function endQuiz (){
-  let scoreInPercentage = (score / parentElem.children.length)* 100;
-  finalScore.classList.add("customClass");
-  finalScore.textContent = `${scoreInPercentage}%`
-  quizSection.style.display = "none";
-}
